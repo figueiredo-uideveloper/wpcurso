222101aa91deabcafb1c65c6ec702343dcc2e692
leandroigd <leandro.figueiredo@klickpages.com.br>
2019-11-08 18:24:46 -0200

    Add theme curso

---
+++ wp-content/themes/curso/index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+    <div class="content-area">
+        <main>
+            <section class="slide"></section>
+            <section class="services"></section>
+            <section class="meddle-area">
+                <aside class="sidebar"></aside>
+                <div class="news"></div>
+            </section>
+            <section class="map"></section>
+        </main>
+    </div>
+
+<?php get_footer(); ?>
