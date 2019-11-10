d244d6e64fabdf99e74e91fdd1b82b1152ae62e7
figueiredo-uideveloper <uideveloper@figueiredo.dev>
2019-11-10 20:12:02 -0300

    Update footer

--- footer.php
+++ footer.php
@@ -1,4 +1,16 @@
-    <footer></footer>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="footer-content-area">
+                    <?php
+                        wp_nav_menu(
+                            array('theme_location' => 'footer_menu')
+                        );
+                    ?>
+                </div>
+            </div>
+        </div>
+    </footer>
     <?php wp_footer(); ?>
 </body>
 </html> 
