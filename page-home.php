d3a59937c2867a92722ecbd2e2961c1d9a15a912
figueiredo-uideveloper <uideveloper@figueiredo.dev>
2019-11-26 18:01:03 -0200

    Set our services section with widgets

--- page-home.php
+++ page-home.php
@@ -11,8 +11,35 @@
         </section>
         <section class="services">
             <div class="container">
+                <h1>Our Services</h1>
                 <div class="row">
-                    <div class="col-12">Serviços</div>
+                    <div class="col-sm-4">
+                        <div class="services-item">
+                            <?php
+                            if (is_active_sidebar('services-1')) {
+                                dynamic_sidebar('services-1');
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="services-item">
+                            <?php
+                            if (is_active_sidebar('services-2')) {
+                                dynamic_sidebar('services-2');
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="services-item">
+                            <?php
+                            if (is_active_sidebar('services-3')) {
+                                dynamic_sidebar('services-3');
+                            }
+                            ?>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
