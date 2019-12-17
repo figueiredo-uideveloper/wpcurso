76f497cb1c10aeda62252dfff1b480605deab447
figueiredo-uideveloper <uideveloper@figueiredo.dev>
2019-12-17 17:24:08 -0200

    Set search

--- search.php
+++ search.php
@@ -2,7 +2,12 @@
     <div id="primary">
         <div id="main">
             <div class="container">
+
+                <h2>Search results for: </h2>
+                <p><strong><?php echo get_search_query(); ?></strong></p>
                 <?php
+                    get_search_form();
+
                     while(have_posts()): the_post();
                         get_template_part('template-parts/content','search');
 
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -2,5 +2,8 @@
     <a href="<?php the_permalink(); ?>">
         <h2 class="title-post"><strong><?php the_title(); ?></strong></h2>
     </a>
+    <?php if(has_category()): ?>
+        <p>Categories: <?php the_category(' '); ?></p>
+    <?php endif; ?>
     <?php the_excerpt(); ?>
 </article>
