5ed46d72b692d7ae76b9c4a2b701cf71bb0fa771
figueiredo-uideveloper <uideveloper@figueiredo.dev>
2019-12-21 18:10:26 -0300

    Set error 404 page

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Curso WordPress</title>
+    <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
 
